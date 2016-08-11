01505c5a1c176a7c4580169b0c08a9f9f61665fb
Kyle Savant <kyle.savant@outlook.com>
2016-08-10 23:01:35 -0500

    Updated the Assembly information for the Data and Service Layers

--- TournamentDrawService/Properties/AssemblyInfo.cs
+++ TournamentDrawService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TournamentDrawService")]
+[assembly: AssemblyTitle("Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TournamentDrawService")]
+[assembly: AssemblyProduct("Service")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
