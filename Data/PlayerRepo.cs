df5731dfad2df5503b0a8e7ea8f8d663c512dd6b
Kyle Savant <kyle.savant@outlook.com>
2016-08-11 22:42:42 -0500

    Testing Dapper.

---
+++ Data/PlayerRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Models;
+
+namespace Data
+{
+    public class PlayerRepo
+    {
+        public List<Player> Get()
+        {
+            using (var connection = Factory.ConnectionFactory.GetOpenConnection())
+            {
+                return connection.Query<Player>("Select * from Players").ToList();
+            }   
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var players = new PlayerRepo().Get();
+            foreach (var player in players)
+            {
+                Console.WriteLine($"First Name: {player.FirstName}");
+                Console.WriteLine($"Last Name: {player.LastName}");
+                Console.WriteLine($"Email: { player.Email}");
+                Console.WriteLine($"Phone: {player.Phone}");
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
